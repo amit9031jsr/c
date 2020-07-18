11adc8cb05ddf364af8500142bc6444261e4f5bf
amit9031jsr <amit9031jsr@gmail.com>
2020-07-18 16:26:38 +0530

    1

--- basics/test.c
+++ basics/test.c
@@ -2,8 +2,24 @@
 #include<conio.h>
 
 int main() {
-    
-    printf("Hello World");
+
+    int a=17, b=4;
+    printf("Sum = %d",a+b);
+    printf("\nDifference = %d",a-b);
+    printf("\nProduct = %d",a*b);
+    printf("\nQuotient = %d",a/b);
+    printf("\nRemainder = %d",a%b);
 
     return 0;
 }
+
+
+/* Output
+
+Sum = 21
+Difference = 13
+Product = 68
+Quotient = 4
+Remainder = 1
+
+*/
