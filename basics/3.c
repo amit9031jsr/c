2bd3f1a0088ac508d90b419030b68daaf0f881e1
amit9031jsr <amit9031jsr@gmail.com>
2020-07-18 17:07:52 +0530

    3

---
+++ basics/3.c
@@ -0,0 +1,25 @@
+// Mixed Mode Arithmetic
+
+#include<stdio.h>
+#include<conio.h>
+
+int main() {
+
+    float a=12, b=2.5;
+    printf("Sum = %.2f\n",a+b);
+    printf("Difference = %.2f\n",a-b);
+    printf("Product = %.2f\n",a*b);
+    printf("a/b = %.2f\n",a/b);
+
+    return 0;
+}
+
+
+/* Output
+
+Sum = 14.50
+Difference = 9.50
+Product = 30.00
+a/b = 4.80
+
+*/
