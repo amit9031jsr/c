029b25a15a09b3f8baed32d810fb96676622fa38
amit9031jsr <amit9031jsr@gmail.com>
2020-07-18 17:01:42 +0530

    1 updated

---
+++ basics/1.c
@@ -0,0 +1,27 @@
+// Integer Arithmetic
+
+#include<stdio.h>
+#include<conio.h>
+
+int main() {
+
+    int a=17, b=4;
+    printf("Sum = %d",a+b);
+    printf("\nDifference = %d",a-b);
+    printf("\nProduct = %d",a*b);
+    printf("\nQuotient = %d",a/b);
+    printf("\nRemainder = %d",a%b);
+
+    return 0;
+}
+
+
+/* Output
+
+Sum = 21
+Difference = 13
+Product = 68
+Quotient = 4
+Remainder = 1
+
+*/
--- basics/test.c
+++ basics/test.c
@@ -3,23 +3,13 @@
 
 int main() {
 
-    int a=17, b=4;
-    printf("Sum = %d",a+b);
-    printf("\nDifference = %d",a-b);
-    printf("\nProduct = %d",a*b);
-    printf("\nQuotient = %d",a/b);
-    printf("\nRemainder = %d",a%b);
+    printf("Hello World");
 
     return 0;
 }
 
-
 /* Output
 
-Sum = 21
-Difference = 13
-Product = 68
-Quotient = 4
-Remainder = 1
+Hello World
 
 */
