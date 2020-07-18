44e8043f0d65d3b07e48cd3d6dde0e649d35c543
amit9031jsr <amit9031jsr@gmail.com>
2020-07-18 17:05:49 +0530

    2

---
+++ basics/2.c
@@ -0,0 +1,25 @@
+// Floating-point Arithmetic
+
+#include<stdio.h>
+#include<conio.h>
+
+int main() {
+
+    float a=12.4, b=3.8;
+    printf("Sum = %.2f\n",a+b);
+    printf("Difference = %.2f\n",a-b);
+    printf("Product = %.2f\n",a*b);
+    printf("a/b = %.2f\n",a/b);
+
+    return 0;
+}
+
+
+/* Output
+
+Sum = 16.20
+Difference = 8.60
+Product = 47.12
+a/b = 3.26
+
+*/
