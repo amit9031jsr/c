1b221493ddb3a0e558f9a0367305d5cbd7bc7823
amit9031jsr <amit9031jsr@gmail.com>
2020-07-21 08:59:04 +0530

    6

---
+++ control_statements/6.c
@@ -0,0 +1,28 @@
+/* while loop
+
+Program to print the numbers from 1 to 10
+
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+int main() {
+
+    int i=1;
+
+    while(i<=10)
+    {
+        printf("%d\t",i);
+        i=i+1;
+    }
+
+    return 0;
+}
+
+
+/* Output
+
+1       2       3       4       5       6       7       8       9       10
+
+*/
