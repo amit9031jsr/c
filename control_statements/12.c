445e891b43746f7966fa68ed39a80877b390a965
amit9031jsr <amit9031jsr@gmail.com>
2020-07-28 13:00:26 +0530

    12

---
+++ control_statements/12.c
@@ -0,0 +1,26 @@
+/*
+Print the numbers from 1 to 10 using do-while
+
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+int main() {
+
+    int i=1;
+
+    do{
+        printf("%d\t",i);
+        i=i+1;
+    }while(i<=10);
+
+    return 0;
+}
+
+
+/* Output
+
+1       2       3       4       5       6       7       8       9       10
+
+*/
